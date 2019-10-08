2355ffedcee671634646d088d951835cc6133810
Kryptonite <saquibedu@gmail.com>
2019-10-08 09:02:22 +0530

    Morris Traversal

--- 23. Binary Trees and BST/intro.cpp
+++ 23. Binary Trees and BST/intro.cpp
@@ -33,11 +33,41 @@ void printPostorder(node* root){
     cout << root -> data << " ";
 }
 
+void printInorder2(node*);
+
 int main(){
-    node* root = buildTree();
+    node* root = buildTreeLevelWise();
     printPreorder(root);
     cout << endl;
     printInorder(root);
     cout << endl;
+    printInorder2(root);
+    cout << endl;
     printPostorder(root);
+}
+
+//8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+void printInorder2(node* n){
+    while(n){
+        if(n->left){
+            node* pre = n->left;
+            while(pre->right and pre->right != n){
+                pre = pre->right;
+            }
+
+            if(pre->right){
+                pre->right = NULL;
+                cout << n->data << " ";
+                n = n->right;
+            }
+            else{
+                pre->right = n;
+                n = n->left;
+            }
+        }
+        else{
+            cout << n->data << " ";
+            n = n->right;
+        }
+    }
 }
